8cdb55792720a1c22f6c66a0ed0655eb6da926e6
albertchan <albert1chan@foxmail.com>
2020-01-30 22:03:11 +0800

    Behavioral Pattern

--- chapter09/chain.py
+++ chapter09/chain.py
@@ -1,52 +1,69 @@
-class Event: 
-    def __init__(self, name): 
-        self.name = name 
+"""
+The Chain of Responsibility Pattern (责任链模式:创建链式对象用来接收广播消息)
 
-    def __str__(self): 
-        return self.name 
+开始介绍行为型设计模式，行为型设计模式处理对象之间的交互和算法问题。
+在责任连模式中，我们把消息发送给一系列对象的首个节点，对象可以选择处理消息或者向下一个对象传递,只有对消息感兴趣的节点处理。
+用来解耦发送者和接收者。在python里通过dynamic dispatching来实现，以一个事件驱动系统来说明：
+"""
 
-class Widget: 
-    def __init__(self, parent=None): 
-        self.parent = parent 
 
-    def handle(self, event): 
-        handler = f'handle_{event}' 
-        if hasattr(self, handler): 
-            method = getattr(self, handler) 
-            method(event) 
-        elif self.parent is not None: 
-            self.parent.handle(event) 
-        elif hasattr(self, 'handle_default'): 
-            self.handle_default(event) 
+class Event:
+    def __init__(self, name):
+        self.name = name
 
-class MainWindow(Widget): 
-    def handle_close(self, event): 
-        print(f'MainWindow: {event}') 
+    def __str__(self):
+        return self.name
 
-    def handle_default(self, event): 
-        print(f'MainWindow Default: {event}') 
 
-class SendDialog(Widget): 
-    def handle_paint(self, event): 
-        print(f'SendDialog: {event}') 
+class Widget:
+    def __init__(self, parent=None):
+        self.parent = parent
 
-class MsgText(Widget): 
-    def handle_down(self, event): 
-        print(f'MsgText: {event}') 
+    def handle(self, event):
+        handler = f'handle_{event}'
+        if hasattr(self, handler):
+            method = getattr(self, handler)
+            method(event)
+        elif self.parent is not None:
+            self.parent.handle(event)
+        elif hasattr(self, 'handle_default'):
+            self.handle_default(event)
+        else:
+            pass
 
-def main(): 
-    mw = MainWindow() 
-    sd = SendDialog(mw) 
-    msg = MsgText(sd) 
 
-    for e in ('down', 'paint', 'unhandled', 'close'): 
-        evt = Event(e) 
-        print(f'Sending event -{evt}- to MainWindow') 
-        mw.handle(evt) 
-        print(f'Sending event -{evt}- to SendDialog') 
-        sd.handle(evt) 
-        print(f'Sending event -{evt}- to MsgText') 
-        msg.handle(evt) 
+class MainWindow(Widget):
+    def handle_close(self, event):
+        print(f'MainWindow: {event}')
 
-if __name__ == '__main__': 
+    def handle_default(self, event):
+        print(f'MainWindow Default: {event}')
+
+
+class SendDialog(Widget):
+    def handle_paint(self, event):
+        print(f'SendDialog: {event}')
+
+
+class MsgText(Widget):
+    def handle_down(self, event):
+        print(f'MsgText: {event}')
+
+
+def main():
+    mw = MainWindow()
+    sd = SendDialog(mw)
+    msg = MsgText(sd)
+
+    for e in ('down', 'paint', 'unhandled', 'close'):
+        evt = Event(e)
+        print(f'Sending event -{evt}- to MainWindow')
+        mw.handle(evt)
+        print(f'Sending event -{evt}- to SendDialog')
+        sd.handle(evt)
+        print(f'Sending event -{evt}- to MsgText')
+        msg.handle(evt)
+
+
+if __name__ == '__main__':
     main()
--- chapter10/command.py
+++ chapter10/command.py
@@ -1,75 +1,97 @@
-import os  
- 
-verbose = True  
+"""
+The Command Pattern (命令模式：用来给应用添加Undo操作)
 
-class RenameFile:
- 
-    def __init__(self, src, dest):  
-        self.src = src 
+命令模式帮助我们把一个操作(undo,redo,copy,paste等)封装成一个对象，通常是创建一个包含Operation所有逻辑和方法的类。 
+通过命令模式可以控制命令的执行时间和过程，还可以用来组织事务。 
+用一些文件操作类来说明命令模式的使用
+"""
+import os
+
+verbose = True
+
+
+class FileCommand:
+    def execute(self):
+        pass
+
+    def redo(self):
+        if verbose:
+            print(f'redo: {self.__class__.__name__}')
+        self.execute()
+
+
+class RenameFile(FileCommand):
+    def __init__(self, src, dest):
+        """ 在init里保存一些必要信息，比如undo需要的老的和新的文件名 """
+        self.src = src
         self.dest = dest
-        
-    def execute(self):  
-        if verbose:  
-            print(f"[renaming '{self.src}' to '{self.dest}']")  
-        os.rename(self.src, self.dest)  
 
-    def undo(self):  
-        if verbose:  
-            print(f"[renaming '{self.dest}' back to '{self.src}']")  
-        os.rename(self.dest, self.src) 
+    def execute(self):
+        if verbose:
+            print(f"[renaming '{self.src}' to '{self.dest}']")
+        os.rename(self.src, self.dest)
 
-class CreateFile:
+    def undo(self):
+        if verbose:
+            print(f"[renaming '{self.dest}' back to '{self.src}']")
+        os.rename(self.dest, self.src)
 
-    def __init__(self, path, txt='hello world\n'):  
-        self.path = path 
-        self.txt = txt 
 
-    def execute(self):  
-        if verbose:  
-            print(f"[creating file '{self.path}']")  
-        with open(self.path, mode='w', encoding='utf-8') as out_file:  
+class CreateFile(FileCommand):
+    def __init__(self, path, txt='hello world\n'):
+        self.path = path
+        self.txt = txt
+
+    def execute(self):
+        if verbose:
+            print(f"[creating file '{self.path}']")
+        with open(self.path, 'w', encoding='utf-8') as out_file:
             out_file.write(self.txt)
-            
-    def undo(self):  
+
+    def undo(self):
         delete_file(self.path)
-        
-class ReadFile:
 
-    def __init__(self, path):  
-        self.path = path 
 
-    def execute(self):  
-        if verbose:  
-            print(f"[reading file '{self.path}']")  
-        with open(self.path, mode='r', encoding='utf-8') as in_file:  
+class ReadFile(FileCommand):
+    def __init__(self, path):
+        self.path = path
+
+    def execute(self):
+        if verbose:
+            print(f"[reading file '{self.path}']")
+        with open(self.path, mode='r', encoding='utf-8') as in_file:
             print(in_file.read(), end='')
-       
-def delete_file(path): 
-    if verbose: 
+
+
+def delete_file(path):
+    if verbose:
         print(f"deleting file {path}")
-    os.remove(path) 
-       
+    os.remove(path)
+
+
 def main():
 
-    orig_name, new_name = 'file1', 'file2'  
- 
-    commands = (CreateFile(orig_name), 
-            ReadFile(orig_name),  
-            RenameFile(orig_name, new_name))
- 
+    orig_name, new_name = 'file1', 'file2'
+
+    commands = (CreateFile(orig_name), ReadFile(orig_name),
+                RenameFile(orig_name, new_name))
+
     [c.execute() for c in commands]
-    
-    answer = input('reverse the executed commands? [y/n] ')  
- 
-    if answer not in 'yY': 
-        print(f"the result is {new_name}")  
-        exit()  
- 
-    for c in reversed(commands):  
-        try:  
-            c.undo()  
-        except AttributeError as e:  
+
+    answer = input('reverse the executed commands? [y/n] ')
+
+    if answer not in 'yY':
+        print(f"the result is {new_name}")
+        exit()
+
+    for c in reversed(commands):
+        try:
+            c.undo()
+        except AttributeError as e:
             print("Error", str(e))
- 
-if __name__ == "__main__":  
+    
+    [c.redo() for c in commands[:2]]
+
+
+if __name__ == "__main__":
     main()
